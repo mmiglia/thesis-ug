de7eae42e8ac256454febcb13f5f0930580f3cb2
pleaseremember <pleaseremember@16abeb15-5767-ec3a-6829-26383b50b651>
2010-04-29 22:22:04 +0000

    Created with Eclipse ADT, using Android SDK version 7 (2.1)

---
+++ trunk/client/src/com/thesisug/Map.java
@@ -0,0 +1,12 @@
+package com.thesisug;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Map extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);        
+    }
+}
