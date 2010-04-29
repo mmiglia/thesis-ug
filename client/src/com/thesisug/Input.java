a79898c64df7393f0490c4da5566ea313bc35251
pleaseremember <pleaseremember@16abeb15-5767-ec3a-6829-26383b50b651>
2010-04-29 22:22:04 +0000

    Created with Eclipse ADT, using Android SDK version 7 (2.1)

---
+++ client/src/com/thesisug/Input.java
@@ -0,0 +1,13 @@
+package com.thesisug;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Input extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input);
+        
+    }
+}
