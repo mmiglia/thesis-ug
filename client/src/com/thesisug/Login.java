7faa593ac6897d616f9b06cd20e7a962f55b7354
pleaseremember <pleaseremember@16abeb15-5767-ec3a-6829-26383b50b651>
2010-04-30 16:21:14 +0000

    messed up with authenticator

--- client/src/com/thesisug/Login.java
+++ client/src/com/thesisug/Login.java
@@ -1,14 +1,27 @@
 package com.thesisug;
 
-import android.app.Activity;
+import android.accounts.AccountAuthenticatorActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Login extends Activity {
+public class Login extends AccountAuthenticatorActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        
+        // Watch for button clicks.
+        Button button = (Button)findViewById(R.id.sign);
+        button.setOnClickListener(buttonListener);
     }
+    
+    private OnClickListener buttonListener = new OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent().setClass(Login.this, ParentTab.class);
+            startActivity(intent);
+        }
+    };
 }
--- client/src/com/thesisug/Todo.java
+++ client/src/com/thesisug/Todo.java
@@ -1,13 +1,27 @@
 package com.thesisug;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Todo extends Activity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todo);
-        
+        // Watch for button clicks.
+        Button button = (Button)findViewById(R.id.getmusic);
+        button.setOnClickListener(mGetMusicListener);
     }
+
+    private OnClickListener mGetMusicListener = new OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivity(Intent.createChooser(intent, "Select picture"));
+        }
+    };
 }
