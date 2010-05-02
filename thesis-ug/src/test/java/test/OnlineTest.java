940e62d3e7cdf85b75781fe34caa209f6a7ff0e5
pleaseremember <pleaseremember@16abeb15-5767-ec3a-6829-26383b50b651>
2010-05-02 14:10:21 +0000

    Update database on concurrency handling, added empty constructor for valueobjects, added catalina.policy configuration, put database folder property on system config.

--- thesis-ug/src/test/java/test/OnlineTest.java
+++ thesis-ug/src/test/java/test/OnlineTest.java
@@ -8,6 +8,8 @@ import junit.framework.TestCase;
 import org.jboss.resteasy.client.ProxyFactory;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,32 +22,74 @@ import businessobject.Configuration;
  */
 public class OnlineTest extends TestCase {
 	Logger log = LoggerFactory.getLogger(OnlineTest.class);
+	// final SimpleClient client = ProxyFactory.create(SimpleClient.class,
+	// "http://localhost:8080/ephemere");
+
 	static {
 		System.setProperty("org.apache.commons.logging.Log",
 				"org.apache.commons.logging.impl.SLF4JLog");
 		System.setProperty("log4j.configuration", "log4j.conf");
 		final Properties CONSTANTS = Configuration.getInstance().constants;
-		if (CONSTANTS.containsKey("http.proxyHost")) System.setProperty("http.proxyHost", CONSTANTS.getProperty("HTTP_PROXY"));
-		if (CONSTANTS.containsKey("http.proxyPort")) System.setProperty("http.proxyPort", CONSTANTS.getProperty("HTTP_PORT"));
-		if (CONSTANTS.containsKey("https.proxyHost")) System.setProperty("https.proxyHost", CONSTANTS.getProperty("HTTPS_PROXY"));
-		if (CONSTANTS.containsKey("https.proxyPort")) System.setProperty("https.proxyPort", CONSTANTS.getProperty("HTTPS_PORT"));
+		if (CONSTANTS.containsKey("http.proxyHost"))
+			System.setProperty("http.proxyHost", CONSTANTS
+					.getProperty("HTTP_PROXY"));
+		if (CONSTANTS.containsKey("http.proxyPort"))
+			System.setProperty("http.proxyPort", CONSTANTS
+					.getProperty("HTTP_PORT"));
+		if (CONSTANTS.containsKey("https.proxyHost"))
+			System.setProperty("https.proxyHost", CONSTANTS
+					.getProperty("HTTPS_PROXY"));
+		if (CONSTANTS.containsKey("https.proxyPort"))
+			System.setProperty("https.proxyPort", CONSTANTS
+					.getProperty("HTTPS_PORT"));
 	}
-	
-	/*public void testLogin(){
-		 RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
-         SimpleClient client = ProxyFactory.create(SimpleClient.class, "http://localhost:8080/ephemere");
-         LoginReply result = client.Authenticate("user", "dummy");
-         log.info(Integer.toString(result.status));
-         assertNotNull(result);
-	}*/
-	
-	public void testEvent(){		
-        SimpleClient client = ProxyFactory.create(SimpleClient.class, "http://localhost:8080/ephemere");
-        List<SingleEvent> result = client.getAllEvents("hell", "cookie");
-        for (SingleEvent o:result){
-        	log.info(o.title+" "+o.description+" "+o.startTime);
-        }
-        assertNotNull(result);
+
+	@Test
+	@Ignore
+	public void testLogin() {
+		RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
+		int i = 0;
+		while (i < 100) {
+			createThread().start();
+			i++;
+		}
+		//SimpleClient client = ProxyFactory.create(SimpleClient.class,
+		//		"http://localhost:8080/ephemere");
+
+		//LoginReply result = client.Authenticate("user2", "dummy");
+		//log.info(Integer.toString(result.status));
+		//assertNotNull(result);
+	}
+
+	private Thread createThread() {
+		Thread result = new Thread() {
+			public void run() {
+				SimpleClient client = ProxyFactory.create(SimpleClient.class,
+						"http://localhost:8080/ephemere");
+
+				LoginReply haha = client.Authenticate("user", "dummy");
+				log.info(Integer.toString(haha.status));
+				try {
+					sleep(Math.round(Math.random() * 100));
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			};
+		};
+		return result;
+	}
+
+	@Ignore("not ready yet")
+	@Test
+	public void testEvent() {
+		SimpleClient client = ProxyFactory.create(SimpleClient.class,
+				"http://localhost:8080/ephemere");
+		List<SingleEvent> result = client.getAllEvents("hell", "cookie");
+		for (SingleEvent o : result) {
+			log.info(o.title + " " + o.description + " " + o.startTime);
+		}
+		assertNotNull(result);
 	}
 
 }
