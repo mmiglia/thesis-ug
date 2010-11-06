1a1a5819e17620e8d82e6495dc1380ed021a7127
thesisUG <thesisUG@16abeb15-5767-ec3a-6829-26383b50b651>
2010-11-06 09:49:18 +0000

    In this branch, the server use a MySQL database instead of the original db4o implementation

---
+++ serverWithMYSQL/src/main/java/businessobject/MapManager.java
@@ -0,0 +1,61 @@
+package businessobject;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import valueobject.Hint;
+import businessobject.google.MapsClient;
+
+/**
+ * This SINGLETON class is the only manager/publisher for map/geolocation services. All implemented
+ * methods are just doing the operation in local database, and then calling all
+ * subsequent methods in the subscriber (3rd party database)
+ * 
+ */
+public class MapManager extends Publisher<MapSubscriber> implements MapInterface{
+	private final static Logger log = LoggerFactory.getLogger(MapManager.class);
+
+	private MapManager() {
+		super();
+	}
+
+	private static class InstanceHolder {
+		private static final MapManager INSTANCE = new MapManager();
+	}
+
+	public static MapManager getInstance() {
+		return InstanceHolder.INSTANCE;
+	}
+
+	/**
+	 * NOT USED. This method was meant to set up an initial configuration
+	 * needed to open connection with the third party servers
+	 * @return boolean false or true upon setting up connection
+	 * 
+	 */
+	public boolean openConnection() {
+		
+		return false;
+	}
+
+	/**
+	 * This method will search for any given 'keyword business' around the given
+	 * location
+	 * 
+	 * @param Return
+	 * @param longitude
+	 *            longitude coordinate from GPS
+	 * @param latitude
+	 *            latitude coordinate from GPS
+	 * @param business
+	 *            keyword of the business
+	 */
+	public List<Hint> searchLocalBusiness(float latitude, float longitude,
+			String business) {
+		log.info("Send search request for query: "+business+" at lat: "+latitude+" lon : "+longitude);
+		MapsClient google = new MapsClient(); // currently there's only google, so we use direct call
+		return google.searchLocalBusiness(latitude, longitude, business);
+	}
+}
---
+++ serverWithMYSQL/src/main/java/exception/TimeOutException.java
@@ -0,0 +1,18 @@
+package exception;
+import java.lang.*;
+/**
+*/
+public class TimeOutException{
+/**
+ * @param message 
+*/
+public TimeOutException(String message) {
+}
+/**
+ * @param message 
+ * @param cause 
+*/
+public TimeOutException(String message, Throwable cause) {
+}
+}
+
