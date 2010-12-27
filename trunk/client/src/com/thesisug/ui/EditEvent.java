0e8db7fb99c8e07e6ede1a447ab2c2c8e11ff110
omegaealfa <omegaealfa@16abeb15-5767-ec3a-6829-26383b50b651>
2010-12-27 12:16:29 +0000

    Aggiunta la funzionalità che impedisce la creazione di un evento con il titolo vuoto o fatto di soli spazi e lo notifica all'utente.

--- trunk/client/src/com/thesisug/ui/EditEvent.java
+++ trunk/client/src/com/thesisug/ui/EditEvent.java
@@ -100,6 +100,16 @@ public class EditEvent extends Activity {
 		save.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				if (!dateIsValid(from, to)) {showDialog(DATE_ERROR_ID); return;}
+				// controlla che il titolo non sia vuoto o contenga solo caratteri white-space
+				if (!titleIsValid(title.getText().toString())) {
+					// comunica che i titoli vuoti non sono gestiti
+						Toast.makeText(getApplicationContext(), R.string.bad_event_name, Toast.LENGTH_SHORT).show();
+					// sembra che in Android non sia possibile imporre il focus su un
+					// elemento dell'interfaccia utente, allora si "richiede" il focus
+					title.requestFocus();
+					// esce (senza inviare nulla)
+					return;
+				}
 				SingleEvent ev;
 				currentDialog = packet.getInt("originator");
 				switch (currentDialog) {
@@ -288,6 +298,12 @@ public class EditEvent extends Activity {
 		return (starting.before(ending));
 	}
 	
+	private boolean titleIsValid(String taskTitle) {
+		// c'è un'occorrenza di .*\\S.* in una stringa se c'è almeno
+		// un carattere non white-space. Per Java i caratteri white-space sono
+		//  \t,\n,\x0B,\f e \r.
+		return taskTitle.matches(".*\\S.*");
+	}
 	private CharSequence getDateString(Calendar cal) {
 		int year = cal.get(Calendar.YEAR);
 		int month = cal.get(Calendar.MONTH);
