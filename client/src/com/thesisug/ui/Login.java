f2ab0b5d45958d96d51e0d1799978e7663363fa8
alessiotoso <alessiotoso@16abeb15-5767-ec3a-6829-26383b50b651>
2011-01-13 15:11:36 +0000

    Registration Support (trunk)

--- client/src/com/thesisug/ui/Login.java
+++ client/src/com/thesisug/ui/Login.java
@@ -114,6 +114,7 @@ public class Login extends AccountAuthenticatorActivity {
         usersettings.edit().putString("ServerURI", serverURI);
         
         NetworkUtilities.changeServerURI(serverURI);
+        
         Button reg = (Button) findViewById(R.id.register);
         reg.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View arg0) {
