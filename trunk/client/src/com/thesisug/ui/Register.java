627f467c355f716f54a24d0de0e3038bb2753f19
alessiotoso <alessiotoso@16abeb15-5767-ec3a-6829-26383b50b651>
2011-01-18 15:55:04 +0000

    Added registration feedback and fixed some bugs in registration procedure

--- trunk/client/src/com/thesisug/ui/Register.java
+++ trunk/client/src/com/thesisug/ui/Register.java
@@ -1,15 +1,21 @@
 package com.thesisug.ui;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.thesisug.R;
-import com.thesisug.communication.LoginResource;
+import com.thesisug.communication.NetworkUtilities;
 import com.thesisug.communication.RegistrationResource;
 
 public class Register extends Activity {
@@ -19,6 +25,7 @@ public class Register extends Activity {
 	private TextView message;
 	private TextView firstnok, lastnok, emailok, newuserok, newpwd1ok, newpwd2ok;
 	private Thread registrationThread;
+	private Handler handler = new MyHandler();
 	
 	/** Called when the activity is first created. */
     @Override
@@ -27,6 +34,8 @@ public class Register extends Activity {
     	super.onCreate(savedInstanceState);
     	
     	setContentView(R.layout.register);
+    	
+    	//Handler handler = new MyHandler();
         
         message = (TextView) findViewById(R.id.submit_message);
         firstnbox = (EditText) findViewById(R.id.firstname);
@@ -49,98 +58,143 @@ public class Register extends Activity {
         newpwd1ok.setVisibility(View.INVISIBLE);
         newpwd2ok.setVisibility(View.INVISIBLE);
         
-        Button sub = (Button) findViewById(R.id.submit);
-        sub.setOnClickListener(new View.OnClickListener() {
-           public void onClick(View arg0) {
-        	   firstnok.setVisibility(View.INVISIBLE);
-               lastnok.setVisibility(View.INVISIBLE);
-               emailok.setVisibility(View.INVISIBLE);
-               newuserok.setVisibility(View.INVISIBLE);
-               newpwd1ok.setVisibility(View.INVISIBLE);
-               newpwd2ok.setVisibility(View.INVISIBLE);
-               
-        	   Log.i(TAG, "Creating new account");
-        	   boolean fieldsok = false;
-        	   boolean pwdok = false;
-        	   
-        	   if (firstnbox.getText().toString().length()==0) {
-        		   fieldsok = false;
-        		   firstnok.setVisibility(View.VISIBLE);
-        	   }
-        	   else
-        		   fieldsok = true;
-        	   
-        	   if (lastnbox.getText().toString().length()==0) {
-        		   fieldsok = false;
-        		   lastnok.setVisibility(View.VISIBLE);
-        	   }
-        	   else
-        		   fieldsok = true;
-        	   
-        	   if (emailbox.getText().toString().length()==0) {
-        		   fieldsok = false;
-        		   emailok.setVisibility(View.VISIBLE);
-        	   }
-        	   else
-        		   fieldsok = true;
-        	   
-        	   if (newuserbox.getText().toString().length()==0) {
-        		   fieldsok = false;
-        		   newuserok.setVisibility(View.VISIBLE);
-        	   }
-        	   else
-        		   fieldsok = true;
-        	   
-        	   if (newpwd1box.getText().toString().length()==0) {
-        		   fieldsok = false;
-        		   newpwd1ok.setVisibility(View.VISIBLE);
-        	   }
-        	   else
-        		   fieldsok = true;
-        	   
-        	   if (newpwd2box.getText().toString().length()==0) {
-        		   fieldsok = false;
-        		   newpwd2ok.setVisibility(View.VISIBLE);
-        	   }
-        	   else {
-        		   fieldsok = true;
-        		   if ( newpwd1box.getText().toString().equals(newpwd2box.getText().toString()) )
-        			   pwdok = true;
-        	   }
-        	   
-        	   if (!fieldsok || !pwdok) {
-        		   if (!fieldsok)
-        			   message.setText(R.string.no_fields);
-        		   else {
-        			   message.setText(R.string.pwd_no_match);
-        			   newpwd1ok.setVisibility(View.VISIBLE);
-        			   newpwd2ok.setVisibility(View.VISIBLE);
-        		   }
-        	   }
-        	   else {
-        		   //si può far partire il thread di registrazione
-        		   message.setText("email: "+emailbox.getText().toString());
-        		   Log.i(TAG,"Starting registrationThread");
-                   registrationThread = RegistrationResource.register(firstnbox.getText().toString(), 
-                		   lastnbox.getText().toString(), 
-                		   emailbox.getText().toString(), 
-                		   newuserbox.getText().toString(), 
-                		   newpwd2box.getText().toString());
-                   Log.i(TAG,"registrationThread returned");
-                   finish();
-                   // TODO 
-        	   }
-        	   
-
-           }
-        });
-        
         Button canc = (Button) findViewById(R.id.cancel);
         canc.setOnClickListener(new View.OnClickListener() {
            public void onClick(View arg0) {
         	   finish();
            }
         });
+    }
+    
+    private class MyHandler extends Handler {
+    	@Override
+        public void handleMessage(Message msg) {
+    		Bundle bundle = msg.getData();
+    		if(bundle.containsKey("status")) {
+    			int value = bundle.getInt("status");
+    			//Toast.makeText(getApplicationContext(), "status:"+value, Toast.LENGTH_SHORT).show();
+    			if (value==0) {
+    				Toast.makeText(getApplicationContext(), R.string.user_exist, Toast.LENGTH_SHORT).show();
+    				newuserbox.setText("");
+    				newuserok.setVisibility(View.VISIBLE);
+    			}
+				if (value==1) {
+					Toast.makeText(getApplicationContext(), R.string.email_exist, Toast.LENGTH_SHORT).show();
+					emailbox.setText("");
+    				emailok.setVisibility(View.VISIBLE);
+				}
+				if (value==2) {
+					Toast.makeText(getApplicationContext(), R.string.registration_ok, Toast.LENGTH_SHORT).show();
+					finish();
+				}
+				if (value==404) {
+					Toast.makeText(getApplicationContext(), R.string.tryConnectionFail, Toast.LENGTH_SHORT).show();
+				}
+    			//view.setText(value);
+    		}
+        }
+
+    }
+    
+    public void register(View view) {
+    	
+    	firstnok.setVisibility(View.INVISIBLE);
+        lastnok.setVisibility(View.INVISIBLE);
+        emailok.setVisibility(View.INVISIBLE);
+        newuserok.setVisibility(View.INVISIBLE);
+        newpwd1ok.setVisibility(View.INVISIBLE);
+        newpwd2ok.setVisibility(View.INVISIBLE);
+        
+ 	   	Log.i(TAG, "Creating new account");
+ 	   	boolean fieldsok = false;
+ 	   	boolean fieldempty = false;        	   
+ 	   	if (!check(firstnbox.getText().toString(), ".*\\S.*")) {
+ 	   		fieldempty = true;
+ 	   		firstnok.setVisibility(View.VISIBLE);
+ 	   	}
+ 	   	if (!check(lastnbox.getText().toString(), ".*\\S.*")) {
+ 	   		fieldempty = true;
+ 	   		lastnok.setVisibility(View.VISIBLE);
+ 	   	}
+ 	   	if (!check(emailbox.getText().toString(), ".*\\S.*")) {
+ 	   		fieldempty = true;
+ 	   		emailok.setVisibility(View.VISIBLE);
+ 	   	}
+ 	   	if (!check(newuserbox.getText().toString(), ".*\\S.*")) {
+ 	   		fieldempty = true;
+ 	   		newuserok.setVisibility(View.VISIBLE);
+ 	   	}
+ 	   	if (!check(newpwd1box.getText().toString(), ".*\\S.*")) {
+ 	   		fieldempty = true;
+ 	   		newpwd1ok.setVisibility(View.VISIBLE);
+ 	   	}
+ 	   	if (!check(newpwd2box.getText().toString(), ".*\\S.*")) {
+ 	   		fieldempty = true;
+ 	   		newpwd2ok.setVisibility(View.VISIBLE);
+ 	   	}
+ 	   	if (fieldempty)
+ 	   		Toast.makeText(getApplicationContext(), R.string.no_fields, Toast.LENGTH_SHORT).show();
+ 	   	else {
+ 	   		// check correctness of email, username and password
+ 	   		if (check(emailbox.getText().toString(), "[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}")) {
+ 	   			if (check(newuserbox.getText().toString(), "\\S{1,}")) {
+ 	   				if (check(newpwd1box.getText().toString(), "\\S{1,}") || 
+ 	   						check(newpwd2box.getText().toString(), "\\S{1,}")) {
+ 	   					// check pwd1=pwd2
+ 	   					if (newpwd1box.getText().toString().equals(newpwd2box.getText().toString())) {
+ 	   						fieldsok = true;
+ 	   					}
+ 	   					else {
+ 	   						newpwd1ok.setVisibility(View.VISIBLE);
+ 	   						newpwd2ok.setVisibility(View.VISIBLE);
+ 	   						newpwd1box.setText("");
+ 	   						newpwd2box.setText("");
+ 	   						Toast.makeText(getApplicationContext(), R.string.pwd_no_match, Toast.LENGTH_SHORT).show();
+ 	   					}   	         					   
+ 	   				}
+ 	   				else {
+ 	   					newpwd1ok.setVisibility(View.VISIBLE);
+ 	   					newpwd2ok.setVisibility(View.VISIBLE);
+ 	   					newpwd1box.setText("");
+ 	   					newpwd2box.setText("");
+ 	   					Toast.makeText(getApplicationContext(), R.string.pwd_invalid, Toast.LENGTH_SHORT).show();
+ 	   				}
+ 	   			}
+ 	   			else {
+ 	   				newuserok.setVisibility(View.VISIBLE);
+ 	   				Toast.makeText(getApplicationContext(), R.string.username_invalid, Toast.LENGTH_SHORT).show();
+ 	   			}
+ 	   		}
+ 	   		else {
+ 	   			emailok.setVisibility(View.VISIBLE);
+ 	   			Toast.makeText(getApplicationContext(), R.string.email_invalid, Toast.LENGTH_SHORT).show();
+ 	   		}
+ 	   	}
+ 	   
+ 	   	if (fieldsok) {
+ 	   		// all fields inserted are ok, start registration thread
+ 	   		message.setText("email: "+emailbox.getText().toString());
+ 	   		Log.i(TAG,"Starting registrationThread");
+ 	   		Toast.makeText(getApplicationContext(), "Sending registration request to "+NetworkUtilities.SERVER_URI, Toast.LENGTH_SHORT).show();
+ 	   		//Handler handler = new MyHandler();
+            registrationThread = RegistrationResource.register(firstnbox.getText().toString(), 
+         		   lastnbox.getText().toString(), 
+         		   emailbox.getText().toString(), 
+         		   newuserbox.getText().toString(), 
+         		   newpwd2box.getText().toString(),
+         		   handler);
+            Log.i(TAG,"registrationThread returned");
+            //finish();
+ 	   }
+    }
+    
+    public boolean check(String input, String regex) {
+    	Pattern pattern = Pattern.compile(regex);
+    	Matcher matcher = pattern.matcher(input);
+    	if (matcher.matches())
+    		return true;
+    	else
+    		return false;
     }
 
 }
