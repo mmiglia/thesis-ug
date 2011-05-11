edc84faf74b897907d7f996daf13754c4ea45b3e
mirk8xr@gmail.com <mirk8xr@gmail.com@16abeb15-5767-ec3a-6829-26383b50b651>
2011-05-11 17:07:08 +0000

    assertion_list_item, create_assertion, details_assertion_item, ViewAssertions

--- trunk/client/src/com/thesisug/ui/ViewAssertions.java
+++ trunk/client/src/com/thesisug/ui/ViewAssertions.java
@@ -1,38 +1,40 @@
 package com.thesisug.ui;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 
 import com.thesisug.R;
 import com.thesisug.communication.AssertionsResource;
-import com.thesisug.communication.valueobject.GroupMember;
 import com.thesisug.communication.valueobject.SingleItemLocation;
+import com.thesisug.ui.Details_assertion_item;
 
 
 
 
-public class ViewAssertions extends ListActivity{
+public class ViewAssertions extends Activity{
 	private static final String TAG ="thesisug - ViewAssertions";
 	private static final int CREATE_ASSERTION=0;	
 	private static final int UPDATE_ASSERTIONS=1;
 	private static final int BACK=2;
+	public final static String ITEM_DATA = "data";
 	
 	private static final int CREATE_GROUP_DIALOG=0;
 	
@@ -45,7 +47,7 @@ public class ViewAssertions extends ListActivity{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.view_assertions);
 		 	
 		//downloadAssertionsThread = AssertionsResource.getAllAssertions(handler, this);
 		//Get join to group request list 
@@ -67,9 +69,8 @@ public class ViewAssertions extends ListActivity{
 		Intent intent;
 		switch (item.getItemId()) {
 		case CREATE_ASSERTION:
-			/*currentDialog=GET_JOIN_USER_GROUP_REQUEST;
-			showDialog(currentDialog);
-			downloadJoinGroupRequestThread = GroupResource.getUserJoinGroupRequest(handler, ManageGroupMenu.this);*/
+			intent = new Intent(getApplicationContext(), Create_Assertion.class);
+			startActivityForResult(intent, 0);
 			break;
 			
 
@@ -84,54 +85,17 @@ public class ViewAssertions extends ListActivity{
 		}
 		return true;
 	}
+	
+
 
 	
 	public void afterAssertionsListLoaded(final List<SingleItemLocation> itemLocationList){
-		//Dismiss dialog
-    	//dismissDialog(GET_JOIN_USER_GROUP_REQUEST);   
-		
 	
-    	//Log.i(TAG,"groupInviteList.size():"+groupInviteList.size());
     	
 		if(itemLocationList==null || itemLocationList.size()==0){
 			Toast.makeText(getApplicationContext(), R.string.noAssertions, Toast.LENGTH_LONG).show();
 		}
-		/*
-		String[] item = new String[20];
-		
 
-		Toast.makeText(getApplicationContext(), R.string.noGroupInviteForUser, Toast.LENGTH_LONG).show();
-		String[] item = new String[20];
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(item[0].toString())
-		    .setCancelable(false)
-		  
-		    .setPositiveButton("Done", new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int id) {
-		        dialog.dismiss();
-		        }
-		    });
-		
-		final AlertDialog alert = builder.create();
-		 alert.show();
-		 
-		
-		int n_list=0;
-		                                
-		 for (SingleItemLocation o : ItemLocationList){
-         	Log.d(TAG,"ItemLocationList in afterAssertionsListLoaded ");
-				// add to the listview
-	
-				item[n_list]= o.item + " -> " + o.location;
-				n_list++;
-			}
-		
-		
-		for (SingleItemLocation o : itemLocationList){
-		
-		Toast.makeText(getApplicationContext(),o.item + "->"+o.location , Toast.LENGTH_SHORT).show();
-		}
-		*/
 		
 		ListView l1 = (ListView) findViewById(R.id.assertionslist);
 		 l1.setAdapter(new assertionsListAdapter(this,itemLocationList));
@@ -141,107 +105,26 @@ public class ViewAssertions extends ListActivity{
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				//Toast.makeText(getBaseContext(), "Details for "+groupMemberList.get(arg2).username, Toast.LENGTH_LONG).show();
 				
-			}
-			 
-		 });
-
-		/*
-		setListAdapter(new ArrayAdapter<SingleItemLocation>(this, R.layout.view_assertions, itemLocationList));
-
-		  ListView lv = getListView();
-		  lv.setTextFilterEnabled(true);
-
-		  lv.setOnItemClickListener(new OnItemClickListener() {
-		    public void onItemClick(AdapterView<?> parent, View view,
-		        int position, long id) {
-		      // When clicked, show a toast with the TextView text
-		      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
-		          Toast.LENGTH_SHORT).show();
-		    }
-		  });
-		
-		  
-		  
-		 ListView l1 = (ListView) findViewById(R.id.assertionslist);
-		 l1.setAdapter(new itemLocationRequestListItemLocationAdapter(this,assertionslist));
-		 
-		 l1.setOnItemClickListener(new OnItemClickListener(){
-
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				Toast.makeText(getBaseContext(), "Details for "+itemLocationList.get(arg2).groupName, Toast.LENGTH_LONG).show();
 				
-			}
-			 
-		 });
-		
-		}
-	
-	/private static class itemLocationRequestListItemLocationAdapter extends BaseAdapter {
-		 private LayoutInflater mInflater;
-
-		 private List<SingleItemLocation> memberList;
-		 
-		 public itemLocationRequestListItemLocationAdapter(Context context,List<SingleItemLocation> list) {
-			 mInflater = LayoutInflater.from(context);
-			 memberList=list;
-
-		 }
-
-		 public View getView(int position, View convertView, ViewGroup parent) {
-			 final ViewHolder holder;
-			 if (convertView == null) {
-				 convertView = mInflater.inflate(R.layout.group_members_list_item, null);
-				 holder = new ViewHolder();
-				 
-				 holder.member=memberList.get(position);
-				 
-				 holder.txtUsername = (TextView) convertView
-				 .findViewById(R.id.txt_member_username);
-
-				 holder.txtUsername.setText(memberList.get(position).username);
-				 
-				 holder.txtJoinDate = (TextView) convertView
-				 .findViewById(R.id.txt_member_joindate);				 
-				 
-				 holder.txtJoinDate.setText(memberList.get(position).joinDate);
-				 
-				 convertView.setTag(holder);
-			 } else {
-				 holder = (ViewHolder) convertView.getTag();
-			 }
+					//LinkedHashMap item = (LinkedHashMap) (arg0.getItemAtPosition(arg2));
+					//SingleItemLocation itemLocation = (SingleItemLocation) item.get(ITEM_DATA);
+					//Toast.makeText(getBaseContext(), "Details for "+itemLocationList.get(arg2).item, Toast.LENGTH_LONG).show();
+					
+					Intent intent = new Intent(getApplicationContext(), Details_assertion_item.class);
+					intent.putExtra("item", itemLocationList.get(arg2).item);
+					intent.putExtra("location", itemLocationList.get(arg2).location);
+					intent.putExtra("username", itemLocationList.get(arg2).username);
+					intent.putExtra("n_views", itemLocationList.get(arg2).n_views);
+					intent.putExtra("n_votes", itemLocationList.get(arg2).n_votes);
+					intent.putExtra("vote", itemLocationList.get(arg2).vote);
+					intent.putExtra("description", itemLocationList.get(arg2).item + " si può trovare in " + itemLocationList.get(arg2).location); 
+					Log.i(TAG,"details_assertion_item:"+ itemLocationList.get(arg2).item + "->" + itemLocationList.get(arg2).location);	
+					startActivityForResult(intent, 0);
 			
-			 return convertView;
-		 }
-
-			 static class ViewHolder {
-				 TextView txtUsername;
-				 TextView txtJoinDate;
-	
-				 GroupMember member;
-			 }
-
-			@Override
-			public int getCount() {
-				
-				return memberList.size();
 			}
-
-			@Override
-			public Object getItem(int position) {
-				return memberList.get(position);
-			}
-
-			@Override
-			public long getItemId(int position) {
-				
-				return Long.parseLong(Integer.toString(memberList.get(position).hashCode()));
-			}*/
-
-
+			 
+		 });
 		 }
 	
 	private static class assertionsListAdapter extends BaseAdapter {
@@ -265,11 +148,16 @@ public class ViewAssertions extends ListActivity{
 				 convertView = mInflater.inflate(R.layout.assertionslist_item, null);
 				 holder = new ViewHolder();
 				 
+				 
 				 holder.itemlocation = itemLocationList.get(position);
 				 
-				 holder.item = (TextView) convertView.findViewById(R.id.item);
+				 holder.txt_item = (TextView) convertView.findViewById(R.id.txt_item);
 
-				 holder.item.setText(itemLocationList.get(position).item +"->"+ itemLocationList.get(position).location );
+				 holder.txt_item.setText("'" +  itemLocationList.get(position).item +"'");
+				 
+				 holder.txt_location = (TextView) convertView.findViewById(R.id.txt_location);
+				 
+				 holder.txt_location.setText(itemLocationList.get(position).location);
 				 
 				 
 				 convertView.setTag(holder);
@@ -281,7 +169,8 @@ public class ViewAssertions extends ListActivity{
 		 }
 
 			 static class ViewHolder {
-				 TextView item;
+				 TextView txt_item;
+				 TextView txt_location;
 				 SingleItemLocation itemlocation;
 			 }
 
