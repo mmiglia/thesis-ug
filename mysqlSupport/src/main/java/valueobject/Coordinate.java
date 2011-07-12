100415e91a7de9d2e271e218a3ba2d843f6fd478
mirk8xr@gmail.com <mirk8xr@gmail.com@16abeb15-5767-ec3a-6829-26383b50b651>
2011-07-12 16:20:11 +0000

    Manage new Place

---
+++ mysqlSupport/src/main/java/valueobject/Coordinate.java
@@ -0,0 +1,17 @@
+package valueobject;
+
+public class Coordinate {
+	
+	String lat;
+	String lng;
+		
+
+	public Coordinate(String lat,String lng) 
+	{
+		
+		this.lat = lat;
+		this.lng = lng;
+	
+	}
+
+}
