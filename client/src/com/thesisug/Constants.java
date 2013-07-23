24974f7abea82c024440d736f1fe472551fbf653
AlbertoServetti@gmail.com <AlbertoServetti@gmail.com@16abeb15-5767-ec3a-6829-26383b50b651>
2013-07-23 13:35:16 +0000

    new default url and version

--- client/src/com/thesisug/Constants.java
+++ client/src/com/thesisug/Constants.java
@@ -22,12 +22,12 @@ public class Constants {
     //public static final String DEFAULT_URL="127.0.0.1";
     
     //Server casa Servetti
-    public static final String DEFAULT_URL = "http://93.50.79.234";
+    public static final String DEFAULT_URL = "http://mizar.cipi.unige.it";
     
     public static final String PROGRAM_NAME="ephemere";
     public static final String DEFAULT_HTTP_PORT="8080";
     public static final String DEFAULT_HTTPS_PORT="8443";
     
-    public static final String VERSION="0.1.2";
+    public static final String VERSION="0.2.0";
     public static final String MIN_SERVER_VER="0.0.2";
 }
